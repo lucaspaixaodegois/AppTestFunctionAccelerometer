ea98c1358d287f9574aeaf2fb08c0e14cd308956
lucas.pg <lucaspaixaodegois@gmail.com>
2018-10-02 03:00:13 -0300

    Criando projeto

---
+++ src/main/java/to/lucaspg/apptestfunctionaccelerometer/TelaPrincipalAccelerometer.java
@@ -0,0 +1,12 @@
+package to.lucaspg.apptestfunctionaccelerometer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TelaPrincipalAccelerometer extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
